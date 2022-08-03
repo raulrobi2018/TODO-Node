b95f1ebf8ef54fa289ab0f8c85d4b881b52574cf
Raul Rodriguez <raulrobi@gmail.com>
2022-08-03 11:30:41 -0300

    Class 45 to 47 - Project init, stdin, stdout, readline, menu

--- app.js
+++ app.js
@@ -1,30 +1,45 @@
-const argv = require("./config/yargs").argv;
-const toDo = require("./to-do/to-do");
+// const argv = require("./config/yargs").argv;
+// const toDo = require("./to-do/to-do");
+require("colors");
 
-const colors = require("colors");
+const {mostrarMenu, pausa} = require("./mensajes");
 
-let comando = argv._[0];
+// let comando = argv._[0];
 
-switch (comando) {
-    case "create":
-        let todo = toDo.create(argv.description);
-        break;
-    case "list":
-        let list = toDo.getList();
-        for (let todo of list) {
-            console.log("==========TODO List===========".green);
-            console.log(todo.description);
-            console.log("Estado", todo.complete);
-            console.log("==============================".green);
-        }
-        break;
-    case "update":
-        let updated = toDo.updateTodo(argv.description, argv.complete);
-        break;
-    case "delete":
-        let deleted = toDo.deleteTodo(argv.description);
-        break;
-    default:
-        console("The command is not valid");
-        break;
-}
+// switch (comando) {
+//     case "create":
+//         let todo = toDo.create(argv.description);
+//         break;
+//     case "list":
+//         let list = toDo.getList();
+//         for (let todo of list) {
+//             console.log("==========TODO List===========".green);
+//             console.log(todo.description);
+//             console.log("Estado", todo.complete);
+//             console.log("==============================".green);
+//         }
+//         break;
+//     case "update":
+//         let updated = toDo.updateTodo(argv.description, argv.complete);
+//         break;
+//     case "delete":
+//         let deleted = toDo.deleteTodo(argv.description);
+//         break;
+//     default:
+//         console.log("The command is not valid");
+//         break;
+// }
+
+console.clear();
+
+const main = async () => {
+    let opt = "";
+    do {
+        //Esperamos a que tengamos resuelto el menú
+        opt = await mostrarMenu();
+        console.log({opt});
+        if (opt !== "7") await pausa();
+    } while (opt !== "7");
+};
+
+main();
